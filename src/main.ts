3e638f5441861cba17fda7ad388ea576b98dbd9f
Cecep Budiman <c3budiman@gmail.com>
2022-07-08 15:01:11 +0700

    fix entity too large

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
   app.use(helmet());
   app.use(compression());
   app.use('/upload', express.static(join(__dirname, '..', 'upload')));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors({
     origin: '*', // temporary
     allowedHeaders: '*',
